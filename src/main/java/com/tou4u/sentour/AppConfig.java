27c740683be4ed48f53d48d01ad81df4cc392344
lms <lms8147@gmail.com>
2018-08-18 14:45:57 +0900

    initial commit

--- src/main/java/com/tou4u/sentour/AppConfig.java
+++ src/main/java/com/tou4u/sentour/AppConfig.java
@@ -5,6 +5,6 @@ public class AppConfig {
     public static final String SK_DEV_API_KEY = "Insert Your API KEY";
     public static final String SK_DEV_API_URL = "https://api2.sktelecom.com/weather/current/hourly";
     public static final String VISITKOREA_API_KEY = "Insert Your API KEY";
-    public static final String VISITKOREA_API_URL = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon";
+    public static final String VISITKOREA_API_URL = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/locationBasedList";
 
 }
